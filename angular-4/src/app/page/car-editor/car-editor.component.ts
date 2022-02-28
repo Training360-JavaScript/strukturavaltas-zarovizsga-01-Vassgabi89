0ebdf5fb07b6958a8a24729780349cbdc97d181b
Gábor Vass <90134776+Vassgabi89@users.noreply.github.com>
2022-02-28 16:59:09 +0100

    próbáltam átírni reaktívra, mert nem ment át a teszten, de már nincs időm

--- angular-4/src/app/page/car-editor/car-editor.component.ts
+++ angular-4/src/app/page/car-editor/car-editor.component.ts
@@ -2,6 +2,7 @@ import { Car } from 'src/app/model/car';
 import { CarService } from 'src/app/service/car.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-car-editor',
@@ -11,6 +12,17 @@ import { Component, OnInit } from '@angular/core';
 export class CarEditorComponent implements OnInit {
 
   car!: Car
+
+  //próbáltam átírni reaktívra, mert nem ment át a teszten, de már nicns időm
+  carFormGroup: FormGroup = new FormGroup({
+    ID: new FormControl(''),
+    model: new FormControl('', [Validators.required, Validators.pattern('^[A-Z0-9][a-zA-Z0-9 ]{1,14}$')]),
+    make: new FormControl('', [Validators.required, Validators.pattern('[A-Z][a-zA-Z]{2,19}')]),
+    year: new FormControl('', [Validators.required, Validators.min(18), Validators.max(100)]),
+    price: new FormControl('', [Validators.required, Validators.min(18), Validators.max(100)]),
+    stock: new FormControl('', [Validators.required, Validators.min(18), Validators.max(100)])
+  })
+
 
   constructor(
     private ar: ActivatedRoute,
